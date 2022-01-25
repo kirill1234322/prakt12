0859d0d4107e48f523c18c2b2f944a3aa366f520
kirill1234322 <kirilltrofimenko757@gmail.com>
2022-01-25 19:18:11 +0500

    menu

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Ввести А\n2. Ввести В\n3. Выполнить операцию +\n4. Выполнить операцию -\n5. Выполнить операцию *\n6. Выполнить операцию /");
+            Console.WriteLine("Выберите операцию ");
+
+
+            int r = Convert.ToInt32(Console.ReadLine());
+            int a = 0, b = 0;
+        }
+    }
+}
