300353d512c8022ef75557d025166a8396471c34
kirill1234322 <kirilltrofimenko757@gmail.com>
2022-01-25 19:21:41 +0500

    B

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,6 +16,11 @@ namespace ConsoleApp1
 
             int r = Convert.ToInt32(Console.ReadLine());
             int a = 0, b = 0;
+
+            if(r == 2)
+            {
+                b = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
